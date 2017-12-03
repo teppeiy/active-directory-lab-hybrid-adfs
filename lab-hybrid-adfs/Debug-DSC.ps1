e184d97e6c531be6338440adcf45605c3e708d9b
Teppei Yamashita <teppeiy@microsoft.com>
2017-12-03 09:30:30 +0900

    1

--- lab-hybrid-adfs/Debug-DSC.ps1
+++ lab-hybrid-adfs/Debug-DSC.ps1
@@ -1,3 +1,6 @@
-﻿Get-AzureRmVMDscExtensionStatus -ResourceGroupName "lab9" -VMName "contosodc"
+﻿while(1){
+Get-AzureRmVMDscExtensionStatus -ResourceGroupName "lab9" -VMName "contosopx1"
+sleep(5)
+}
 
 Get-AzureRmVMDscExtension -ResourceGroupName "lab9" -VMName "contosodc" | Select-Object -ExpandProperty Properties
--- lab-hybrid-adfs/Deploy-Lab.ps1
+++ lab-hybrid-adfs/Deploy-Lab.ps1
@@ -1,2 +1,5 @@
-﻿New-AzureRmResourceGroup -Name "lab4" -Location "Southeast Asia"
-New-AzureRmResourceGroupDeployment -ResourceGroupName "lab4" -deploymentNumber 5 -TemplateFile ".\FullDeploy.json" -TemplateParameterFile ".\azuredeploy.parameters.json" -Verbose
+﻿New-AzureRmResourceGroup -Name "lab9" -Location "Southeast Asia"
+New-AzureRmResourceGroupDeployment -ResourceGroupName "lab9" -deploymentNumber 9 -TemplateFile ".\FullDeploy.json" -TemplateParameterFile ".\azuredeploy.parameters.json" -Verbose
+
+New-AzureRmResourceGroup -Name "lab8" -Location "Southeast Asia"
+New-AzureRmResourceGroupDeployment -ResourceGroupName "lab8" -deploymentNumber 7 -adfsImageSKU "2008-R2-SP1" -TemplateFile ".\FullDeploy.json" -TemplateParameterFile ".\azuredeploy.parameters.json" -Verbose
