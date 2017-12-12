47af3a60df534760d82f9ce78fc0b406bf6e5e26
Teppei Yamashita <teppeiy@microsoft.com>
2017-12-12 22:15:10 +0900

    CustomScriptExtension typeHandlerVersion=1.8

--- lab-hybrid-adfs/Deploy-Lab.ps1
+++ lab-hybrid-adfs/Deploy-Lab.ps1
@@ -8,30 +8,38 @@ $TemplateParameterFile = "C:\Users\teppeiy\source\repos\active-directory-lab-hyb
 #adImageSKU/adfsImageSKU "allowedValues": [ "2016-Datacenter", "2012-R2-Datacenter", "2008-R2-SP1" ]
 
 # Win2008R2
-$deploymentNumber = 3
+$deploymentNumber = 4
 $ResourceGroupName = "Forest-$deploymentNumber"
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location "Southeast Asia"
 New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateFile -TemplateParameterFile $TemplateParameterFile `
 	-deploymentNumber $deploymentNumber -adImageSKU "2008-R2-SP1" -adfsImageSKU "2008-R2-SP1" -adDomainMode "Win2008R2" -adForestMode "Win2008R2" -Verbose
 
 # Win2012 - Verified
-$deploymentNumber = 4
+$deploymentNumber = 5
 $ResourceGroupName = "Forest-$deploymentNumber"
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location "Southeast Asia"
 New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateFile -TemplateParameterFile $TemplateParameterFile `
 	-deploymentNumber $deploymentNumber -adImageSKU "2012-R2-Datacenter" -adfsImageSKU "2012-R2-Datacenter" -adDomainMode "Win2012" -adForestMode "Win2012" -Verbose
 
 # Win2012R2
-$deploymentNumber = 5
+$deploymentNumber = 3
 $ResourceGroupName = "Forest-$deploymentNumber"
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location "Southeast Asia"
 New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateFile -TemplateParameterFile $TemplateParameterFile `
 	-deploymentNumber $deploymentNumber -adImageSKU "2012-R2-Datacenter" -adfsImageSKU "2012-R2-Datacenter" -adDomainMode "Win2012R2" -adForestMode "Win2012R2" -Verbose
 
+# Win2016
+$deploymentNumber = 3
+$ResourceGroupName = "Forest-$deploymentNumber"
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location "Southeast Asia"
+New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateFile -TemplateParameterFile $TemplateParameterFile `
+	-deploymentNumber $deploymentNumber -adImageSKU "2016-Datacenter" -adfsImageSKU "2016-Datacenter" -adDomainMode "Win2012R2" -adForestMode "Win2012R2" -Verbose
+
+
 
 Get-AzureRmResourceGroup | Select ResourceGroupName
 
-Remove-AzureRmResourceGroup -Name "Forest-3" -force
+Remove-AzureRmResourceGroup -Name "Forest-6" -force
 
 # Debug Script
 $ResourceGroupName = "Forest-5"
