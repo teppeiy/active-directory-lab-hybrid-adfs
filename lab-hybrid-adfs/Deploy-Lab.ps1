0fd8f826a53e0f0a1f421cf950e61e9824be215c
Teppei Yamashita <teppeiy@microsoft.com>
2017-12-19 00:02:04 +0900

    Fixed JsonADDomainExtension

--- lab-hybrid-adfs/Deploy-Lab.ps1
+++ lab-hybrid-adfs/Deploy-Lab.ps1
@@ -73,23 +73,25 @@ New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -Templa
 
 
 
+$TemplateFile = "C:\Users\teppeiy\source\repos\active-directory-lab-hybrid-adfs\lab-hybrid-adfs\FullDeploy.json"
+$TemplateParameterFile = "C:\Users\teppeiy\source\repos\active-directory-lab-hybrid-adfs\lab-hybrid-adfs\azuredeploy.parameters.json"
+
+
 # Win2012 - 
-$deploymentNumber = 7
+$deploymentNumber = 4
 $ResourceGroupName = "Forest-$deploymentNumber"
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location "Southeast Asia"
 New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateFile -TemplateParameterFile $TemplateParameterFile `
 	-deploymentNumber $deploymentNumber -adImageSKU "2012-R2-Datacenter" -adfsImageSKU "2012-R2-Datacenter" -adDomainMode "Win2012" -adForestMode "Win2012" -Verbose
 
 # Win2008R2
-$deploymentNumber = 8
+$deploymentNumber = 3
 $ResourceGroupName = "Forest-$deploymentNumber"
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location "Southeast Asia"
 New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateFile -TemplateParameterFile $TemplateParameterFile `
 	-deploymentNumber $deploymentNumber -adImageSKU "2008-R2-SP1" -adfsImageSKU "2008-R2-SP1" -adDSCConfigurationFunction "DomainController2k8r2" -adfsDSCConfigurationFunction "ADFS2k8r2" -adDomainMode "Win2008R2" -adForestMode "Win2008R2" -Verbose
 
 
-$TemplateFile = "C:\Users\teppeiy\source\repos\active-directory-lab-hybrid-adfs\lab-hybrid-adfs\FullDeploy.json"
-$TemplateParameterFile = "C:\Users\teppeiy\source\repos\active-directory-lab-hybrid-adfs\lab-hybrid-adfs\azuredeploy.parameters.json"
 
 
 #New-AzureRmResourceGroup -Name "lab9" -Location "Southeast Asia"
