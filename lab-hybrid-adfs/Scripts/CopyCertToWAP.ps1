85bf79fff1480dc6f60ed5553e9e2905cab4c2c6
Teppei Yamashita <teppeiy@microsoft.com>
2017-12-19 16:43:10 +0900

    Cleaned up spaces

--- lab-hybrid-adfs/Scripts/CopyCertToWAP.ps1
+++ lab-hybrid-adfs/Scripts/CopyCertToWAP.ps1
@@ -1,20 +1,15 @@
 ﻿param (
     [Parameter(Mandatory)]
     [string]$DCFQDN,
-
     [Parameter(Mandatory)]
     [string]$adminuser,
-
     [Parameter(Mandatory)]
     [string]$password,
-
-	[Parameter(Mandatory)]
-	[int]$instance,
-
-	[Parameter(Mandatory)]
-	[string]$WapFqdn,
-
-	[Parameter(Mandatory)]
+    [Parameter(Mandatory)]
+    [int]$instance,
+    [Parameter(Mandatory)]
+    [string]$WapFqdn,
+    [Parameter(Mandatory)]
     [string]$adfsImageSKU
     #"allowedValues": [ "2016-Datacenter", "2012-R2-Datacenter", "2008-R2-SP1" ]
 )
@@ -22,13 +17,13 @@
 $ErrorActionPreference = "Stop"
 $arr = $DCFQDN.split('.')
 $DomainName = $arr[1]
-$SecPW=ConvertTo-SecureString $password -AsPlainText -Force
-$File=$null
-$Subject=$null
+$SecPW = ConvertTo-SecureString $password -AsPlainText -Force
+$File = $null
+$Subject = $null
 
 [System.Management.Automation.PSCredential]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($adminuser)", $SecPW)
 
-$completeFile="c:\temp\prereqsComplete"
+$completeFile = "c:\temp\prereqsComplete"
 md "c:\temp" -ErrorAction Ignore
 md "c:\AADLab" -ErrorAction Ignore
 
@@ -37,7 +32,7 @@ New-Item -ItemType File $log
 
 
 if (!(Test-Path -Path "$($completeFile)0")) {
-    $PathToCert="\\$DCFQDN\src"
+    $PathToCert = "\\$DCFQDN\src"
     net use "\\$DCFQDN\src" $password /USER:$adminuser
     Copy-Item -Path "$PathToCert\*.pfx" -Destination "c:\temp\" -Recurse -Force
     Copy-Item -Path "$PathToCert\*.cer" -Destination "c:\temp\" -Recurse -Force
@@ -47,47 +42,46 @@ if (!(Test-Path -Path "$($completeFile)0")) {
 
 if (!(Test-Path -Path "$($completeFile)1")) {
 
-	    if ($adfsImageSKU -eq "2008-R2-SP1") {
+    if ($adfsImageSKU -eq "2008-R2-SP1") {
         # Win2008R2
         "In 2008-R2-SP1 path: $adfsImageSKU" >> $log
-			}
-	else{
-		        # Win2012 or above
-	#install root cert
-    $RootFile  = Get-ChildItem -Path "c:\temp\*.cer"
-    $RootPath  = $RootFile.FullName
-    $rootCert  = Import-Certificate -CertStoreLocation Cert:\LocalMachine\Root -FilePath $RootPath
+    }
+    else {
+        # Win2012 or above
+        #install root cert
+        $RootFile = Get-ChildItem -Path "c:\temp\*.cer"
+        $RootPath = $RootFile.FullName
+        $rootCert = Import-Certificate -CertStoreLocation Cert:\LocalMachine\Root -FilePath $RootPath
 
-	#install the certificate that will be used for ADFS Service
-    $CertFile  = Get-ChildItem -Path "c:\temp\*.pfx"
-	for ($file=0;$file -lt $CertFile.Count;$file++)
-	{
-		$Subject   = $CertFile[$file].BaseName
-		$CertPath  = $CertFile[$file].FullName
-		$cert      = Import-PfxCertificate -Exportable -Password $SecPW -CertStoreLocation cert:\localmachine\my -FilePath $CertPath
-	}
+        #install the certificate that will be used for ADFS Service
+        $CertFile = Get-ChildItem -Path "c:\temp\*.pfx"
+        for ($file = 0; $file -lt $CertFile.Count; $file++) {
+            $Subject = $CertFile[$file].BaseName
+            $CertPath = $CertFile[$file].FullName
+            $cert = Import-PfxCertificate -Exportable -Password $SecPW -CertStoreLocation cert:\localmachine\my -FilePath $CertPath
+        }
 
-	$Subject = $WapFqdn -f $instance
-    $cert      = Get-ChildItem Cert:\LocalMachine\My | where {$_.Subject -eq "CN=$Subject"} -ErrorAction SilentlyContinue
+        $Subject = $WapFqdn -f $instance
+        $cert = Get-ChildItem Cert:\LocalMachine\My | where {$_.Subject -eq "CN=$Subject"} -ErrorAction SilentlyContinue
 
-    Install-WebApplicationProxy `
-        -FederationServiceTrustCredential $DomainCreds `
-        -CertificateThumbprint $cert.Thumbprint`
-        -FederationServiceName $Subject
-		}
+        Install-WebApplicationProxy `
+            -FederationServiceTrustCredential $DomainCreds `
+            -CertificateThumbprint $cert.Thumbprint`
+            -FederationServiceName $Subject
+    }
     #record that we got this far
     New-Item -ItemType file "$($completeFile)1"
 }
 
 if (!(Test-Path -Path "$($completeFile)2")) {
 
-		    if ($adfsImageSKU -eq "2008-R2-SP1") {
+    if ($adfsImageSKU -eq "2008-R2-SP1") {
         # Win2008R2
         "In 2008-R2-SP1 path: $adfsImageSKU" >> $log
-			}
-	else{
-	$Subject = $WapFqdn -f $instance
-	$str = @"
+    }
+    else {
+        $Subject = $WapFqdn -f $instance
+        $str = @"
 #https://blogs.technet.microsoft.com/rmilne/2015/04/20/adfs-2012-r2-web-application-proxy-re-establish-proxy-trust/
 `$DomainCreds = Get-Credential
 `$File      = Get-ChildItem -Path "c:\temp\*.pfx"
@@ -103,10 +97,10 @@ Install-WebApplicationProxy ``
 Start-Service -Name appproxysvc
 "@
 
-	$scriptBlock = [Scriptblock]::Create($str)
-	$scriptBlock.ToString() | out-file C:\AADLab\resetWAPTrust.ps1
+        $scriptBlock = [Scriptblock]::Create($str)
+        $scriptBlock.ToString() | out-file C:\AADLab\resetWAPTrust.ps1
 
-		}
+    }
     #record that we got this far
     New-Item -ItemType file "$($completeFile)2"
 }
@@ -114,27 +108,27 @@ Start-Service -Name appproxysvc
 if (!(Test-Path -Path "$($completeFile)3")) {
     # Shortcuts
 
-	$WshShell = New-Object -comObject WScript.Shell
-	$dt="C:\Users\Public\Desktop\"
-	$ieicon="%ProgramFiles%\Internet Explorer\iexplore.exe, 0"
-	$Subject = $WapFqdn -f $instance
+    $WshShell = New-Object -comObject WScript.Shell
+    $dt = "C:\Users\Public\Desktop\"
+    $ieicon = "%ProgramFiles%\Internet Explorer\iexplore.exe, 0"
+    $Subject = $WapFqdn -f $instance
 
-	$links = @(
-		@{site="http://connect.microsoft.com/site1164";name="Azure AD Connect Home";icon=$ieicon},
-		@{site="https://docs.microsoft.com/en-us/azure/active-directory/connect/active-directory-aadconnect";name="Azure AD Docs";icon=$ieicon},
-		@{site="https://$Subject/adfs/ls/idpinitiatedsignon.aspx";name="ADFS IDP Signon";icon=$ieicon},
-		@{site="%windir%\system32\WindowsPowerShell\v1.0\PowerShell_ISE.exe";name="PowerShell ISE";icon="%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell_ise.exe, 0"},
-		@{site="%windir%\system32\services.msc";name="Services";icon="%windir%\system32\filemgmt.dll, 0"},
-		@{site="%windir%\system32\RAMgmtUI.exe";name="Remote Access Management";icon="%SystemRoot%\System32\damgmtres.dll, 0"},
-		@{site="c:\AADLab";name="AAD Lab Files";icon="%windir%\explorer.exe, 13"}
-	)
+    $links = @(
+        @{site = "http://connect.microsoft.com/site1164"; name = "Azure AD Connect Home"; icon = $ieicon},
+        @{site = "https://docs.microsoft.com/en-us/azure/active-directory/connect/active-directory-aadconnect"; name = "Azure AD Docs"; icon = $ieicon},
+        @{site = "https://$Subject/adfs/ls/idpinitiatedsignon.aspx"; name = "ADFS IDP Signon"; icon = $ieicon},
+        @{site = "%windir%\system32\WindowsPowerShell\v1.0\PowerShell_ISE.exe"; name = "PowerShell ISE"; icon = "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell_ise.exe, 0"},
+        @{site = "%windir%\system32\services.msc"; name = "Services"; icon = "%windir%\system32\filemgmt.dll, 0"},
+        @{site = "%windir%\system32\RAMgmtUI.exe"; name = "Remote Access Management"; icon = "%SystemRoot%\System32\damgmtres.dll, 0"},
+        @{site = "c:\AADLab"; name = "AAD Lab Files"; icon = "%windir%\explorer.exe, 13"}
+    )
 
-	foreach($link in $links){
-		$Shortcut = $WshShell.CreateShortcut("$($dt)$($link.name).lnk")
-		$Shortcut.TargetPath = $link.site
-		$Shortcut.IconLocation = $link.icon
-		$Shortcut.Save()
-	}
+    foreach ($link in $links) {
+        $Shortcut = $WshShell.CreateShortcut("$($dt)$($link.name).lnk")
+        $Shortcut.TargetPath = $link.site
+        $Shortcut.IconLocation = $link.icon
+        $Shortcut.Save()
+    }
 
     #record that we got this far
     New-Item -ItemType file "$($completeFile)3"
